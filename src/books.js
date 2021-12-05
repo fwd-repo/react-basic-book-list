5bae56a848b2672f5826ecca3c5a0dfa8aa734a4
Ed Tansey <office.fwd@yahoo.com>
2021-12-05 23:45:28 +0000

    Add project files

---
+++ src/books.js
@@ -0,0 +1,28 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/51wAK2Ya9HS._SY346_.jpg',
+    title: 'The Taste of Ginger: A Novel',
+    author: 'Mansi Shah',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/41Ix9rQzApL._SY346_.jpg',
+    title:
+      'Through the Banks of the Red Cedar: My Father and the Team That Changed the Game',
+    author: 'Maya Washington',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/51a5mzEp4jL._SY346_.jpg',
+    title:
+      "Murder at Teal's Pond: Hazel Drew and the Mystery That Inspired Twin Peaks",
+    author: 'David Bushman',
+  },
+  {
+    id: 4,
+    img: 'https://m.media-amazon.com/images/I/51IZSx0nW8L._SY346_.jpg',
+    title: 'The Intangible: A Novel',
+    author: 'C.J. Washington',
+  },
+]
